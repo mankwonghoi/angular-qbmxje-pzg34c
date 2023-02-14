e2aca82cc2750c8eb295d42150a452e4bfd20681
harrisonlaw98 <124799224+harrisonlaw98@users.noreply.github.com>
2023-02-14 00:31:09 -0500

    2 files modified

--- src/app/app-table/app-table.component.ts
+++ src/app/app-table/app-table.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  columnDefinition,
-  TabId
-} from '../ColumnDefinition';
+import { columnDefinition, TabId } from '../ColumnDefinition';
 import { User } from '../Data/User';
 import { TableControlService } from '../table-control.service';
 
@@ -18,29 +15,23 @@ export class AppTableComponent implements OnInit {
   _columnDefinition: any[] | undefined;
   _viewSelected = TabId.User;
   isEditMode: boolean = false;
-
+  activeId: number = this.tableControlService.selectedTab;
 
   searchText: string | undefined;
 
-  constructor(public tableControlService: TableControlService) { }
+  constructor(public tableControlService: TableControlService) {}
 
   ngOnInit(): void {
     //Assign current selected Tab
     this._viewSelected = TabId.User; //to be change to selected tab...
     //Get columnDefinition by selected Tab
 
-
-
     this.refreshTable();
-
   }
 
   changeMode() {
     this.isEditMode = !this.isEditMode;
-
   }
-
-
 
   refreshTable() {
     this._columnDefinition = columnDefinition.filter(
@@ -52,12 +43,14 @@ export class AppTableComponent implements OnInit {
     }
 
     //clone obejct without reference
-    this._tableViewData = this._data?.map(e => e);
+    this._tableViewData = this._data?.map((e) => e);
   }
 
+  public inputChange(event: any, key: any) {}
 
-
-  public inputChange(event: any, key: any) {
-
+  onClick(): void {
+    console.log(this.activeId);
+    this._viewSelected = this.activeId;
+    this.refreshTable();
   }
 }
